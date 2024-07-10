8b7151761f376fc3950594a80f6cc39334fd3cdf
Sejal Sinha <sejalsinha322@gmail.com>
2024-07-10 11:57:01 +0530

    Added profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,25 @@
 import { Button } from "@mui/material";
+import { auth } from "../Firebase";
+import {  useNavigate } from 'react-router-dom';
 
 
 const Profile = () => {
+  const navigate = useNavigate();
+
   const name = localStorage.getItem('name');
   const email = localStorage.getItem('email');
   const profilePic = localStorage.getItem('profilePic');
+
+  async function handleLogout() {
+    try {
+      await auth.signOut();
+      localStorage.removeItem("user");
+      navigate("/app/login");
+      console.log("User logged out successfully!");
+    } catch (error) {
+      console.error("Error logging out:", error.message);
+    }
+  }
 
   return (
     <div className="profile">
@@ -15,19 +30,25 @@ const Profile = () => {
            src={profilePic} 
            alt="Profile"  
            width={"40%"}
-           style={{borderRadius: "50%"}}
+           style={{borderRadius: "50%", marginLeft: "80px", marginBottom: "20px"}}
         />
         }
-        <h1>Welcome {name}</h1>
-        <h1>Name: {name}</h1>
-        <h1>Email: {email}</h1>
+        <h1 style={{fontSize: "25px", fontFamily: "sans-serif", textAlign: "center", fontWeight: "bolder", marginBottom: "20px"}}>
+            Welcome {name} !!
+        </h1>
+        <h1 style={{fontFamily: "sans-serif", color: "gray", fontSize: "17px"}}>
+            Name: {name}
+        </h1>
+        <h1 style={{fontFamily: "sans-serif", color: "gray", fontSize: "17px"}}>
+            Email: {email}
+        </h1>
     </div>
 
     <div>
     <Button 
      variant="contained" 
      fullWidth
-    //  onClick={handleSubmit}
+     onClick={handleLogout}
     //  disabled = {submitButtonDisabled}
     > 
      Log Out
